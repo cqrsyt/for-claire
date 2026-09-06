015a36ac2fd4658dac114223719a00afc86a6b55
cqrsyt <u3648221@connect.hku.hk>
2026-09-06 06:09:34 -0700

    Deploy js/config.js with basePath /for-claire/

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@
  * To change password: sha256("your-password") and paste here; or ask me.
  */
 window.SITE_CONFIG = {
-  basePath: "", // default: root (Cloudflare / custom domain)
+  basePath: "/for-claire/", // GitHub Pages project site
   sitePasswordHash: "310f089018032ba50b85d43ed54f70c81cf8e8a4846b26bc2037274e56f1acd1",
   passwordStorageKey: "claire-album-unlocked",
 };
