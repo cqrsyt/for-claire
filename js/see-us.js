b43f2e7134be5687eb067e58a47c7c59a235a222
cqrsyt <u3648221@connect.hku.hk>
2026-09-12 04:09:45 -0700

    Put See us back inside the story card so it can be tapped.

--- js/see-us.js
+++ js/see-us.js
@@ -2,7 +2,6 @@
   function bind() {
     var btn = document.getElementById("btn-see-us");
     if (!btn) return;
-    if (btn.parentElement !== document.body) document.body.appendChild(btn);
     btn.onclick = function () {
       if (window.claireOpenAlbum) window.claireOpenAlbum();
     };
